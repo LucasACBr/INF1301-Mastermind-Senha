510472cae8a4db6eecdd23535329d582f4bb02be
LucasACBr <lucas@projetoacbr.com.br>
2021-12-01 14:03:58 -0300

    Create funcoesLogicaSenha.cpython-39.pyc

---
+++ senha/main.py
@@ -0,0 +1,20 @@
+from model import model
+from view import view
+from controller import controller
+from tkinter import *
+import random
+from functools import partial
+
+def main():
+    
+    # Variáveis do Jogo      
+    venceu = False
+    listaJogadas = []
+    entrada = []
+    # Início de Fato da Partida
+    view.inicializa()
+            
+    return 0
+
+main()
+
